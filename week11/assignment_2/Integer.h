3d93ccd27e4f6b1d39fedee97547cefae08cd673
metrosxxmin <sxxmin.lee@gmail.com>
2019-11-15 23:44:48 +0900

    create: assignment_2 & related header-file.

---
+++ week11/assignment_2/Integer.h
@@ -0,0 +1,32 @@
+//
+// Created by Soomin Lee on 2019/11/15.
+//
+
+#ifndef INTEGER_H
+#define INTEGER_H
+
+#include "Number.h"
+
+class Integer : public Number {
+private:
+    static int parse_string(const std::string& str);
+    int value;
+public:
+    Integer();
+    explicit Integer(int _value);
+    explicit Integer(const std::string& str);
+
+    Number* add(Number* num);
+    Number* sub(Number* num);
+    Number* mul(Number* num);
+    Number* div(Number* num);
+
+    void set_val(int val);
+    int val() const;
+    type types() const;
+    std::string to_string() const;
+};
+
+
+#endif // INTEGER_H
+
